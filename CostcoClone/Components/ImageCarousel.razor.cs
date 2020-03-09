b555ab3be45658797b66a2332f8f485005034560
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-03-08 23:02:58 -0400

    a

--- CostcoClone/Components/ImageCarousel.razor.cs
+++ CostcoClone/Components/ImageCarousel.razor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CostcoClone.Components
 {
-    public partial class ImageCarousel : ComponentBase
+    public partial class ImageCarousel : ComponentBase, IDisposable
     {
         [Parameter]
         public List<string> ImageURLs { get; set; } = new List<string> 
@@ -23,11 +23,51 @@ namespace CostcoClone.Components
             "content/d-20w0725-fq-200302-travel.webp"
         };
 
-        public int Index { get; set; }
+        private int _index;
+        public int Index 
+        { 
+            get
+            {
+                return _index;
+            }
+            set
+            {
+                if (value >= ImageURLs.Count) value = 0;
+                else if (value < 0) value = ImageURLs.Count - 1;
+
+                _index = value;
+            }
+        }
+
+        public bool Alive { get; set; } = true;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Spin();
+        }
 
         public string IsSelectedCSS(int index)
         {
             return Index == index ? "background-color: blue;" : "";
+        }
+
+        private async Task Spin()
+        {
+            while(Alive)
+            {
+                await Task.Delay(5000);
+                Index++;
+                await InvokeAsync(StateHasChanged);
+            }
+
+            return;
+        }
+
+        public void Dispose()
+        {
+            Alive = false;
         }
     }
 }
