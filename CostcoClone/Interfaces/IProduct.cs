bd8f74758a05c759845cd49f54958d5208138f09
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-05-24 14:32:17 -0400

    s

---
+++ CostcoClone/Interfaces/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Interfaces
+{
+    public interface IProduct
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Categories { get; set; }
+        public List<string> Tags { get; set; }
+        public int Stock { get; set; }
+    }
+}
