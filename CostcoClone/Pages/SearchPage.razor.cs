f178f2a2702ec222ac3549bbb7b6ef1aba381abb
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-02 18:12:56 -0400

    Commit

---
+++ CostcoClone/Pages/SearchPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using CostcoClone.Repository;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Pages
+{
+    public partial class SearchPage : ComponentBase
+    {
+        [Inject]
+        public IProductRepository ProductRepository { get; set; }
+        [Parameter]
+        public string Department { get; set; } = "All";
+        [Parameter]
+        public string Query { get; set; } = "";
+    }
+}
