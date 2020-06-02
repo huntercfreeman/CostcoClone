7fe8b0f7ceb8239f66cab6441900c70823979e68
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-02 19:29:35 -0400

    w

---
+++ CostcoClone/Models/Products/Computers/Laptop.cs
@@ -0,0 +1,37 @@
+﻿using CostcoClone.Interfaces;
+using CostcoClone.Interfaces.Departments;
+using CostcoClone.Repository;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Models.Products.Computers
+{
+    public class Laptop : IProduct, IComputers
+    {
+        public Laptop(string title, decimal price, int stock, MarkupString productDetails, IProductRepository productRepository, List<string> imageURLs)
+        {
+            Title = title;
+            Price = price;
+            Categories = new List<string> { "Computers", "Laptop" };
+            Tags = new List<string> { "Computers", "Laptop" };
+            Stock = stock;
+            ProductDetails = productDetails;
+            ProductId = Guid.NewGuid().ToString();
+            ImageURLs = imageURLs;
+
+            productRepository.AddComputer(this);
+        }
+
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public List<string> Categories { get; set; }
+        public List<string> Tags { get; set; }
+        public int Stock { get; set; }
+        public MarkupString ProductDetails { get; set; }
+        public string ProductId { get; set; }
+        public List<string> ImageURLs { get; set; }
+    }
+}
--- CostcoClone/Repository/IProductRepository.cs
+++ CostcoClone/Repository/IProductRepository.cs
@@ -10,6 +10,7 @@ namespace CostcoClone.Repository
     public interface IProductRepository
     {
         public void AddAppliance(IAppliance appliance);
+        public void AddComputer(IComputers computer);
         public IList<IProduct> GetProducts();
         public IList<IProduct> GetProductsByTitle(string title);
         public IProduct GetProductById(string productId);
