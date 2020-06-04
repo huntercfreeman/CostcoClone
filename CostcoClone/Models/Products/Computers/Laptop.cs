328424c66bcfa769d768851d0a74002113f8468b
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-04 16:53:55 -0400

    a

--- CostcoClone/Models/Products/Computers/Laptop.cs
+++ CostcoClone/Models/Products/Computers/Laptop.cs
@@ -4,6 +4,7 @@ using CostcoClone.Repository;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,7 +34,16 @@ namespace CostcoClone.Models.Products.Computers
         public MarkupString ProductDetails { get; set; }
         public string ProductId { get; set; }
         public List<string> ImageURLs { get; set; }
+        public List<string> Features { get; set; }
         public string Resolution { get; set; }
-        public int MyProperty { get; set; }
+        public string GraphicCard { get; set; }
+        public Color Color { get; set; }
+        public string Brand { get; set; }
+        public string ComputerType { get; set; }
+        public string HardDriveSize { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Processor { get; set; }
+        public string ScreenSize { get; set; }
+        public string ScreenType { get; set; }
     }
 }
