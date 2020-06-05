c154ca51ba476ff89cdee6d4cb57744074fecc98
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-05 17:18:52 -0400

    a

--- CostcoClone/Interfaces/Departments/Computers/ILaptopsNotebookComputers.cs
+++ CostcoClone/Interfaces/Departments/Computers/ILaptopsNotebookComputers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,17 @@ namespace CostcoClone.Interfaces.Departments.Computers
     public interface ILaptopsNotebookComputers
     {
         public int MyProperty { get; set; }
-        public enum Features
-        {
-            Backlit Keyboard,
-
-        }
+        public List<string> Features { get; set; }
+        public string DeliveryType { get; set; }
+        public string Resolution { get; set; }
+        public string GraphicCard { get; set; }
+        public Color Color { get; set; }
+        public string Brand { get; set; }
+        public string ComputerType { get; set; }
+        public string HardDriveSize { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Processor { get; set; }
+        public string ScreenSize { get; set; }
+        public string ScreenType { get; set; }
     }
 }
--- CostcoClone/Models/Products/Computers/LaptopNotebookComputer.cs
+++ CostcoClone/Models/Products/Computers/LaptopNotebookComputer.cs
@@ -1,6 +1,7 @@
 ﻿using CostcoClone.Interfaces.Departments.Computers;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace CostcoClone.Models.Products.Computers
 {
     public class LaptopNotebookComputer : ILaptopsNotebookComputers
     {
+        public int MyProperty { get; set; }
+        public List<string> Features { get; set; }
+        public string DeliveryType { get; set; }
+        public string Resolution { get; set; }
+        public string GraphicCard { get; set; }
+        public Color Color { get; set; }
+        public string Brand { get; set; }
+        public string ComputerType { get; set; }
+        public string HardDriveSize { get; set; }
+        public string OperatingSystem { get; set; }
+        public string Processor { get; set; }
+        public string ScreenSize { get; set; }
+        public string ScreenType { get; set; }
     }
 }
