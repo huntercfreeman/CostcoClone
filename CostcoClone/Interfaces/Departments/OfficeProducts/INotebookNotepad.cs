9f852c16c9cc71155438fe07cbdde3b58e630c6e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-06 18:02:00 -0400

    w

---
+++ CostcoClone/Interfaces/Departments/OfficeProducts/INotebookNotepad.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Interfaces.Departments.OfficeProducts
+{
+    interface INotebookNotepad
+    {
+    }
+}
