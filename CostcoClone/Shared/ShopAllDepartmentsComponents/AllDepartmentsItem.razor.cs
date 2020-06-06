5dcb719882d85215055dccdb08be79af1dd1543c
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-06 13:28:43 -0400

    s

---
+++ CostcoClone/Shared/ShopAllDepartmentsComponents/AllDepartmentsItem.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Shared.ShopAllDepartmentsComponents
+{
+    public partial class AllDepartmentsItem : ComponentBase
+    {
+        [Parameter]
+        public EventCallback<RenderFragment> ExpansionCallbackOver { get; set; }
+        [Parameter]
+        public EventCallback<RenderFragment> ExpansionCallbackOut { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string Title { get; set; }
+    }
+}
