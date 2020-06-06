eb25e0c1f5ba542f3efeed15bcbbeddaa16c6621
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-06 07:33:41 -0400

    a

---
+++ CostcoClone/Components/FilterMenuItem.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostcoClone.Components
+{
+    public partial class FilterMenuItem : ComponentBase
+    {
+        [Parameter]
+        public bool IsCollapsed { get; set; } = true;
+    }
+}
