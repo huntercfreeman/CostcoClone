1574c33628470d4e2a0175b52a40f6ebf8d57994
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-06-06 13:11:35 -0400

    a

--- CostcoClone/Interfaces/Departments/JewelryWatchesSunglasses/IOneOfAKindRing.cs
+++ CostcoClone/Interfaces/Departments/JewelryWatchesSunglasses/IOneOfAKindRing.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CostcoClone.Interfaces.Departments.JewelryWatchesSunglasses
 {
-    interface IOneOfAKindRing
+    interface IOneOfAkindRing
     {
     }
 }
